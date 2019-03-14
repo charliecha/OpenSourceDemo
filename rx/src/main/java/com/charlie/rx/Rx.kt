5aa16cc59d61e4296e45864cdf38dece0a6b4270
zhazongxun <zhazongxun@baidu.com>
2019-03-15 00:17:00 +0800

    Update Rx.kt

--- rx/src/main/java/com/charlie/rx/Rx.kt
+++ rx/src/main/java/com/charlie/rx/Rx.kt
@@ -1,14 +1,47 @@
 package com.charlie.rx
 
-import io.reactivex.Flowable
-import io.reactivex.Observable
-import io.reactivex.ObservableOnSubscribe
-import io.reactivex.Observer
+import io.reactivex.*
 import io.reactivex.disposables.Disposable
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
 fun main() {
+//    testObservable()
+//    testFlowable()
+    testOpt()
+}
+
+private fun testOpt() {
+    val observable = Observable.create(ObservableOnSubscribe<Int>() { emitter ->
+        emitter.onNext(1)
+        emitter.onNext(2)
+        emitter.onNext(3)
+        emitter.onNext(4)
+        emitter.onComplete()
+    })
+
+    val observer = object : Observer<Int> {
+        override fun onNext(t: Int) {
+            println("onNext $t")
+        }
+
+        override fun onComplete() {
+            println("onComplete")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            println("onSubscribe")
+        }
+
+        override fun onError(e: Throwable) {
+            println("onError $e")
+        }
+    }
+
+    observable.map { 2 * it }.subscribe(observer)
+}
+
+private fun testObservable() {
     Observable.just("Hello world").subscribe { word ->
         println("got $word @${Thread.currentThread().name}")
     }
@@ -40,7 +73,9 @@ fun main() {
     }
 
     observable.subscribe(observer)
+}
 
+private fun testFlowable() {
     Flowable.range(0, 10).subscribe {
         println(it)
     }
@@ -66,5 +101,4 @@ fun main() {
             println("onComplete")
         }
     })
-
 }
